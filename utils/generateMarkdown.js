25b743ce312a4320696424446ff8fa6d4661070a
Corey Post <corey.post0120@gmail.com>
2020-07-24 16:45:17 -0600

    basic layout of code for generate markdown and updated gitignore

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,50 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ![${data.licenseType}](https://img.shields.io/badge/License-${encodeURI(data.licenseType)}-blue.svg)
+
+  This README file was dynamically created from the Command Line.
+
+  # Description
+
+  ${data.}
+
+  # Table of Contents
+  
+  * []()
+  * []()
+  * []()
+  * []()
+  * []()
+  * []()
+
+  # Installation
+
+  ${data.}
+
+  # Usage
+
+  ${data.}
+
+  # License
+
+  ${data.}
+
+  # Contributing
+
+  ${data.}
+
+  # Tests
+
+  ${data.}
+
+  # Questions
+
+  GitHub Username: []() 
+  Email: ${data.}
+  ${data.}
 `;
 }
 
